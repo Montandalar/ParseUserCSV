be4fb1ae6c7b54f4c934c57fc9fd6a170b34b6bb
Montandalar <jbis1337@hotmail.com>
2020-01-31 03:39:40 +0000

    Connect to the given database, with error messages

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,8 @@
 <?php
 
+define("EXIT_BAD_INVOCATION", 1);
+define("EXIT_DATABASE_ERROR", 2);
+
 function print_usage() {
 ?>
 user_upload.php - Parse a CSV of users into the database
@@ -8,9 +11,10 @@ user_upload.php - Parse a CSV of users into the database
     --create_table - this will cause the MySQL users table to be built
     (and no further action will be taken)
     --dry_run - don't alter the database
-    -u - MySQL database username
-    -p - MySQL database password
-    -h - MySQL database host
+    -u - MySQL username
+    -p - MySQL password
+    -h - MySQL host
+    -d - MySQL database
     --help - show this help
 <?php
 }
@@ -19,22 +23,51 @@ function main() {
     global $argv;
     global $argc;
 
-    $opts = getopt("u:p:h:", ["file::", "create_table", "dry_run::", "help::"],
+    $opts = getopt("u:p:h:d:", ["file::", "create_table", "dry_run::", "help::"],
         $optind);
 
     //var_dump($opts);
     if (isset($opts["help"])) {
         print_usage();
-        exit(1);
+        exit(EXIT_BAD_INVOCATION);
     }
 
-    foreach (["u" => "username", "p" => "password", "h" => "host"] as $opt => $msg) {
+    // Not the approach to use if the application would be localised, but it
+    // is quicker to write if the only language will be english
+    foreach (["u" => "username", "p" => "password",
+              "h" => "host", "d" => "database"]
+                as $opt => $msg)
+    {
         if (!isset($opts[$opt])) {
-            printf("Please specify the %s for the database\n", $msg);
+            printf("Please specify the %s for MySQL", $msg);
             print_usage();
-            exit(1);
+            exit(EXIT_BAD_INVOCATION);
         }
     }
+    
+    // Open the database connection
+    try {
+        $dbh = new PDO(
+            sprintf('mysql:host=%s;dbname=%s', $opts['h'], $opts['d']),
+            $opts['u'],
+            $opts['p']);
+    } catch (PDOException $e) {
+        echo "Error connecting to database: ", $e->getMessage(), "\n";
+        if ($e->getCode() == 1049) {
+            echo "You should create the database first before using it with user_upload.php\n";
+        } else if ($e->getCode() == 1044) {
+            echo "Make sure your database user has permissions to use the database\n";
+        }
+        exit(EXIT_DATABASE_ERROR);
+    }
+    // If the option --create_table was given do that now then quit
+
+    // If the table does not exist, quit now
+    
+    // Load the CSV
+
+    // Do the database insertion
+
 }
 main();
 ?>
